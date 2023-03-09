e569e72ac5ad86b29a7068792ee8464f47edfacc
Nikita Petrykin <nvnedu2120@gmail.com>
2023-03-09 23:54:44 +0100

    feat: Length, append & insert methods
    Added 3 methods for DoublyLinkedList class:
    - length() -> Returns the length of the linked list
    - append() -> Adds a new element to the end of the list
    - insert() -> Inserts element at index

--- linked_list.py
+++ linked_list.py
@@ -1,29 +1,68 @@
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 # Doubly linked list node class
 class Node:
     def __init__(self, data: Any) -> None:
         self.data = data
-        self.prev = None
         self.next = None
+        self.prev = None
 
 
 # Doubly linked list class
 class DoublyLinkedList:
     def __init__(self) -> None:
-        pass
+        self.head = None
+        self.tail = None
+        self.count = 0
 
     # Returns the length of the linked list
+    @property
     def length(self) -> int:
-        pass
+        return self.count
 
     # Adds a new element to the end of the list
     def append(self, data: Any) -> None:
-        pass
+        new_node = Node(data)
+
+        if self.count == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.count += 1
 
     # Inserts element at index
     def insert(self, data: Any, index: int) -> None:
-        pass
+        if index < 0 or index > self.count:
+            raise IndexError("Index out of range")
+
+        new_node = Node(data)
+
+        if self.count == 0:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        elif index == self.count:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            current = self.head
+            for i in range(index):
+                current = current.next
+            
+            new_node.prev = current.prev
+            new_node.next = current
+            current.prev.next = new_node
+            current.prev = new_node
+
+        self.count += 1
 
     # Removes an element by index and returns it
     def delete(self, index: int) -> Any:
@@ -38,7 +77,7 @@ class DoublyLinkedList:
         pass
 
     # Returns a copy of the current list
-    def clone(self) -> DoublyLinkedList:
+    def clone(self) -> List:
         pass
     
     # Reverses the list
@@ -58,5 +97,14 @@ class DoublyLinkedList:
         pass
     
     # Appends all elements of the given list to the end of the current one
-    def extend(self, ex_list: DoublyLinkedList) -> None:
+    def extend(self, ex_list: List) -> None:
         pass
+    
+    # Returns the string representation of the object
+    def __str__(self) -> str:
+        current = self.head
+        elements = []
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        return " -> ".join(elements)
